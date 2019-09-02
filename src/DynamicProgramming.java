b0fd6ce0934b6fe851405795671e716a9a0c9e2e
Sky-Titan <er65119@naver.com>
2019-09-02 16:26:34 +0900

    first

---
+++ src/DynamicProgramming.java
@@ -0,0 +1,123 @@
+
+public class DynamicProgramming {
+
+	public DynamicProgramming() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	static void cuttingStick ()//동적 프로그래밍 - 막대기 자르기 문제
+	{
+		int p[] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};// p[i]는 i 길이 막대기의 가격 i = 0 ~ 10
+		
+		System.out.println(R(p,7));
+	}
+	
+	static void stoneTable () //p275
+	{
+		int n = 8;
+		int map[][] = {
+				{6,7,12,-5,5,3,11,3},
+				{-8,10,14,9,7,13,8,5},
+				{11,12,7,4,8,-2,9,4}
+		};
+		
+		int max[] = {0,0,0,0};
+		int[] pattern = new int[8];
+		
+		for(int i=0;i<4;i++)
+		{
+			pattern[7] = i; 
+			for(int j=6;j>=0;j--)
+			{
+				if(j+1 == 0) //첫번째자리에 돌
+				{
+					if(map[1][j] + map[0][j+1] > map[2][j] + map[0][j+1])
+						pattern[j] = 1;
+					else
+						pattern[j] = 2;
+				}
+				else if(j+1 == 1)//두번째자리에 돌
+				{
+					if(map[0][j] + map[1][j+1] > map)
+				}
+			}
+		}
+		
+	}
+	
+	static int R(int[] p,int n)//R2(n)은 n 길이의 막대기의 최대가격 - bottom up
+	{
+		int max=0;
+		int r[]=new int[p.length];
+		
+		for(int i=0;i<r.length;i++)
+			r[i]=0;
+		
+		for(int j=0;j<=n;j++)
+		{
+			max=0;
+			for(int i=0;i<=j;i++)
+			{
+				max = Math.max(max, p[i] + r[j-i]);
+			}
+			r[j] = max;
+		}
+		return r[n];
+	}
+	
+	static void findingRoute() //동적 프로그래밍 - 책 p270 bottom-up
+	{
+		int n = 4;
+		int map[][] = {
+				{0,0,0,0,0},
+				{0,6,7,12,5},
+				{0,5,3,11,18},
+				{0,7,17,3,3},
+				{0,8,10,14,9},
+		};
+		
+		int max[][] = {
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+				{0,0,0,0,0},
+		};
+		
+		max[1][1] = map[1][1];
+		for(int i=2;i<=n;i++)
+		{
+			max[i][1] = max[i-1][1] + map[i][1];
+		}
+		
+		for(int i=2;i<=n;i++)
+		{
+			max[1][i] = max[1][i-1] + map[1][i];
+		}
+		
+		for(int j=2;j<=n;j++)
+		for(int i=2;i<=n;i++)
+		{
+			max[j][i] = Math.max(max[j-1][i] + map[j][i], max[j][i-1] + map[j][i]);
+		}
+		
+		System.out.println(max[n][n]);
+	}
+	
+	static int matrixPath(int i,int j) //동적 프로그래밍 - 책 p270 top-down (중복호출o! - 안좋음)
+	{
+		int map[][] = {
+				{0,0,0,0,0},
+				{0,6,7,12,5},
+				{0,5,3,11,18},
+				{0,7,17,3,3},
+				{0,8,10,14,9},
+		};
+		if(i==0 || j==0)
+			return 0;
+		else
+			return map[i][j] + Math.max(matrixPath(i-1, j), matrixPath(i, j-1));
+	}
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+public class Main {
+	
+	static long[] a;
+	
+	static int swap=0;
+	
+	static void mergeSort(int p,int r)
+	{
+		if(p<r)
+		{
+			int q= (p+r)/2;
+			mergeSort(p, q);
+			mergeSort(q+1, r);
+			merge(p, q, r);
+		}
+	}
+	static void merge(int p,int q,int r)
+	{
+		int i=p,j=q+1;
+		int t=1;
+		long[] temp = new long[a.length];
+		
+		while(i<=q && j<=r)
+		{
+			if(a[i] > a[j])
+			{
+				temp[t++] = a[j++];
+				swap++;
+			}
+			else
+				temp[t++] = a[i++];
+		}
+		while(i<=q)
+			temp[t++] = a[i++];
+		while(j<=r)
+			temp[t++] = a[j++];
+		
+		i = p;
+		t = 1;
+		while(i<=r)
+			a[i++]=temp[t++];
+	}
+	
+	static void bj1517() throws Exception
+	{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bf.readLine());
+		int i=1;
+		a= new long[n+1];
+		String line = bf.readLine();
+		StringTokenizer str = new StringTokenizer(line, " ");
+		while(str.hasMoreTokens())
+		{
+			a[i] = Integer.parseInt(str.nextToken());
+			i++;
+		}
+		mergeSort(1, a.length-1);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		if(swap>0)
+			swap++;
+		bw.write(swap+"\n");
+		bw.flush();
+		bw.close();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		DynamicProgramming dp = new DynamicProgramming();
+		
+		
+	}
+	
+}
