3f248c5717daa4162e88d4a0e68e228ed1a42fd6
Sky-Titan <er65119@naver.com>
2020-07-31 22:56:26 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,19 @@
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 
-	public static int[] solution(int[] prices) {
-		int[] answer = new int[prices.length];
+	public static int solution(int[] citations) {
+		int answer = 0;
 
-		Queue<Integer> queue = new LinkedList<>();
-		
-		for(int i=0;i< prices.length;i++)
+		//quickSort(citations,0, citations.length-1);
+
+		Arrays.sort(citations);
+
+		for(int i = citations.length-1;i >= 0;i--)
 		{
-			int now = prices[i];
-
-			if(!queue.isEmpty())
-			{
-				int size = queue.size();
-
-				for(int j=0;j<size;j++)
-				{
-					int index = queue.poll();
-
-					answer[index]++;
-
-					//주식 가격이 떨어지는 시점이 오면 큐에 더이상 넣지 않는다.
-					if(prices[index] <= now)
-						queue.offer(index);
-				}
-			}
-			queue.offer(i);
+			if(citations.length - i >= citations[i])
+				return citations.length - i;
 		}
 
 		return answer;
@@ -42,15 +22,11 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		int[] prices={1,2,3,2,3};
+		int[] citations = {3, 0, 6, 1, 5};
 
-		solution(prices);
+		System.out.println(solution(citations));
 	}
 
-	static void printForArray(Object[] o)
-	{
-		for(int i=0;i<o.length;i++)
-			System.out.println(o[i]);
-	}
+
 	
 }
