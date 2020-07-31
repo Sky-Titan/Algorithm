28bbe9402b748eafb9702d1d8afca973e411e7ac
Sky-Titan <er65119@naver.com>
2020-07-31 17:33:42 +0900

    프로그래머스

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,56 @@
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
-import java.awt.List;
+
+
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 public class Main {
 
-	
+	public static int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
 
-	
-	public static void main(String[] args) {
+		Queue<Integer> queue = new LinkedList<>();
 		
-		
-		 String p = "hi2(3(xm)co)hi";
-		 
-		 int num = 2;
-		 
-		 int mark[] = {1,2,3,4,5,6,5};
-		 System.out.println(Solution.solution(num,mark));
+		for(int i=0;i< prices.length;i++)
+		{
+			int now = prices[i];
+
+			if(!queue.isEmpty())
+			{
+				int size = queue.size();
+
+				for(int j=0;j<size;j++)
+				{
+					int index = queue.poll();
+
+					answer[index]++;
+
+					//주식 가격이 떨어지는 시점이 오면 큐에 더이상 넣지 않는다.
+					if(prices[index] <= now)
+						queue.offer(index);
+				}
+			}
+			queue.offer(i);
+		}
+
+		return answer;
 	}
-	
+
+
+
+	public static void main(String[] args) {
+		int[] prices={1,2,3,2,3};
+
+		solution(prices);
+	}
+
+	static void printForArray(Object[] o)
+	{
+		for(int i=0;i<o.length;i++)
+			System.out.println(o[i]);
+	}
 	
 }
---
+++ src/StringProcess.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class StringProcess {
+
+    static void bj1593() throws Exception
+    {
+        BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer strtok = new StringTokenizer(b.readLine());
+        int g = Integer.parseInt(strtok.nextToken());
+        int s = Integer.parseInt(strtok.nextToken());
+
+
+        String W = b.readLine();
+        String S = b.readLine();
+
+        char[] w_array = W.toCharArray();
+        char[] s_array = S.toCharArray();
+
+
+
+
+
+    }
+
+}
