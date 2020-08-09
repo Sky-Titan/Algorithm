52e6c86d285616759a5787582089339cf329e150
Sky-Titan <er65119@naver.com>
2020-08-09 22:20:53 +0900

    2020카카오 외벽점검 완료

--- src/Solution.java
+++ src/Solution.java
@@ -7,71 +7,45 @@ public class Solution {
 	static int[] dist_a;
 	static int min_people;
 	static int visit_count = 0;
-	static boolean visited[], friend_visited[];
+	static boolean visited[];
+	static int FRIEND_NUMBER;//친구들 숫자
 
-
-	static int wall[];
 
 	public static int solution(int n, int[] weak, int[] dist) {
-		int answer = 0;
 
 		N = n;
 		weak_a = weak;
 		dist_a = dist;
 
-		wall = new int[n];
+		FRIEND_NUMBER = dist.length;
 
-		int current = weak[0];
-		int next = 0;
-
-		int value = 1;
-
-		friend_visited = new boolean[dist.length];
-
-		/*
-		for(int i = 0;i < weak.length;i++)
-		{
-			if(i < weak.length-1)
-				next = weak[i+1];
-			else
-				next = n;
-
-
-			for(int j = current;j < next;j++)
-			{
-				wall[j] = value;
-			}
-
-			current = next;
-			value++;
-		}
-*/
 		visited = new boolean[weak.length];
 
-		min_people = friend_visited.length+1;
+		min_people = FRIEND_NUMBER+1;
 
 		//한계 인원 결정
-		for(int people = 1; people <= friend_visited.length;people++)
+		for(int people = 1; people <= FRIEND_NUMBER;people++)
 		{
 			//투입 인원 결정
-			for(int friend = friend_visited.length-1; friend >= 0;friend--)
+			for(int friend = FRIEND_NUMBER-1; friend >= 0;friend--)
 			{
 				//시작 위치 결정
-				for(int start = 0; start< weak_a.length; start++)
+				for(int start = 0; start < weak_a.length; start++)
 				{
-					friend_visited[friend] = true;
-					clockVisit(start, dist_a[friend]);
-					dfs(people, friend, 1);
+					int current = clockVisit(start, dist_a[friend]);
+					dfs(people, friend, 1, current);
 					clockUnvisit(start, dist_a[friend]);
 
 					//최소 인원 찾음
 					if(min_people <= people)
 						return min_people;
+				}
 
-					counterClockVisit(start, dist_a[friend]);
-					dfs_counter(people, friend, 1);
+				for(int start = weak_a.length-1 ; start >= 0; start--)
+				{
+					int current = counterClockVisit(start, dist_a[friend]);
+					dfs_counter(people, friend, 1, current);
 					counterClockUnvisit(start, dist_a[friend]);
-					friend_visited[friend] = false;
 
 					//최소 인원 찾음
 					if(min_people <= people)
@@ -84,10 +58,10 @@ public class Solution {
 	}
 
 
-	public static void dfs(int peopleLimit, int index, int peopleCount)
+	public static void dfs(int peopleLimit, int start_friend, int peopleCount, int current)
 	{
 		//한계 인원 됨
-		if(peopleLimit == peopleCount || isCompleteVisit())
+		if(peopleLimit == peopleCount)
 		{
 			//완전 방문 완료
 			if(isCompleteVisit())
@@ -98,35 +72,29 @@ public class Solution {
 		else
 		{
 			//그 다음 친구 결정
-			for(int friend = friend_visited.length-1;friend >=0; friend--)
+			for(int friend = start_friend-1;friend >=0; friend--)
 			{
-				if(!friend_visited[friend])
+				for(int start = current; start < weak_a.length; start++)
 				{
-					for(int start = 0; start < weak_a.length; start++)
+					if(!visited[start])
 					{
-						if(!visited[start])
-						{
-							friend_visited[friend] = true;
-							clockVisit(start, dist_a[friend]);
-							dfs(peopleLimit, friend, peopleCount+1);
-							clockUnvisit(start, dist_a[friend]);
-							friend_visited[friend] = false;
+						int current2 = clockVisit(start, dist_a[friend]);
+						dfs(peopleLimit, friend, peopleCount+1, current2);
+						clockUnvisit(start, dist_a[friend]);
 
-							if(min_people <= peopleLimit)
-								return;
-						}
+						if(min_people <= peopleLimit)
+							return;
 					}
 				}
-
 			}
 		}
 
 	}
 
-	public static void dfs_counter(int peopleLimit, int index, int peopleCount)
+	public static void dfs_counter(int peopleLimit, int start_friend, int peopleCount, int current)
 	{
 		//한계 인원 됨
-		if(peopleLimit == peopleCount || isCompleteVisit())
+		if(peopleLimit == peopleCount)
 		{
 			//완전 방문 완료
 			if(isCompleteVisit())
@@ -137,23 +105,18 @@ public class Solution {
 		else
 		{
 			//그 다음 친구 결정
-			for(int friend = friend_visited.length-1; friend >= 0; friend--)
+			for(int friend = start_friend-1; friend >= 0; friend--)
 			{
-				if(!friend_visited[friend])
+				for(int start = current; start >=0; start--)
 				{
-					for(int start = 0; start < weak_a.length; start++)
+					if(!visited[start])
 					{
-						if(!visited[start])
-						{
-							friend_visited[friend] = true;
-							counterClockVisit(start, dist_a[friend]);
-							dfs_counter(peopleLimit, friend, peopleCount+1);
-							counterClockUnvisit(start, dist_a[friend]);
-							friend_visited[friend] = false;
+						int current2 = counterClockVisit(start, dist_a[friend]);
+						dfs_counter(peopleLimit, friend, peopleCount+1, current2);
+						counterClockUnvisit(start, dist_a[friend]);
 
-							if(min_people <= peopleLimit)
-								return;
-						}
+						if(min_people <= peopleLimit)
+							return;
 					}
 				}
 			}
@@ -170,6 +133,7 @@ public class Solution {
 	}
 
 
+	//시계방향 방문
 	public static int clockVisit(int start, int distance)
 	{
 		int current = start+1;
@@ -178,14 +142,13 @@ public class Solution {
 		if(start == weak_a.length - 1)
 			current = 0;
 
-		int count = 0;
-
 		setVisit(start);
 
 		while(true)
 		{
 			int move = weak_a[current] - weak_a[former];
 
+			//원형 순환
 			if(move < 0)
 				move += N;
 
@@ -193,25 +156,23 @@ public class Solution {
 			{
 				distance -= move;
 				setVisit(current);
-				count++;
+
+				former = current;
 
 				if(current == weak_a.length-1)
 					current = 0;
 				else
 					current++;
 
-				if(former == weak_a.length-1)
-					former = 0;
-				else
-					former++;
 			}
 			else
 				break;
 		}
 
-		return count;
+		return current;
 	}
 
+	//시계방향 방문 해제
 	public static int clockUnvisit(int start, int distance)
 	{
 		int current = start+1;
@@ -220,14 +181,13 @@ public class Solution {
 		if(start == weak_a.length - 1)
 			current = 0;
 
-		int count = 0;
-
 		setUnvisit(start);
 
 		while(true)
 		{
 			int move = weak_a[current] - weak_a[former];
 
+			//원형 순환
 			if(move < 0)
 				move += N;
 
@@ -235,26 +195,22 @@ public class Solution {
 			{
 				distance -= move;
 				setUnvisit(current);
-				count++;
+
+				former = current;
 
 				if(current == weak_a.length-1)
 					current = 0;
 				else
 					current++;
-
-				if(former == weak_a.length-1)
-					former = 0;
-				else
-					former++;
 			}
 			else
 				break;
 		}
 
-		return count;
+		return current;
 	}
 
-	//반시계방향
+	//반시계방향 방문
 	public static int counterClockVisit(int start, int distance)
 	{
 		int current = start-1;
@@ -263,14 +219,13 @@ public class Solution {
 		if(start == 0)
 			current = weak_a.length-1;
 
-		int count = 0;
-
 		setVisit(start);
 
 		while(true)
 		{
 			int move = weak_a[former] - weak_a[current];
 
+			//원형 순환
 			if(move < 0)
 				move += N;
 
@@ -278,25 +233,22 @@ public class Solution {
 			{
 				distance -= move;
 				setVisit(current);
-				count++;
+
+				former = current;
 
 				if(current == 0)
 					current = weak_a.length-1;
 				else
 					current--;
-
-				if(former == 0)
-					former = weak_a.length-1;
-				else
-					former--;
 			}
 			else
 				break;
 		}
 
-		return count;
+		return current;
 	}
 
+	//반시계방향 방문 해제
 	public static int counterClockUnvisit(int start, int distance)
 	{
 		int current = start-1;
@@ -305,14 +257,13 @@ public class Solution {
 		if(start == 0)
 			current = weak_a.length-1;
 
-		int count = 0;
-
 		setUnvisit(start);
 
 		while(true)
 		{
 			int move = weak_a[former] - weak_a[current];
 
+			//원형 순환
 			if(move < 0)
 				move += N;
 
@@ -320,23 +271,19 @@ public class Solution {
 			{
 				distance -= move;
 				setUnvisit(current);
-				count++;
+
+				former = current;
 
 				if(current == 0)
 					current = weak_a.length-1;
 				else
 					current--;
-
-				if(former == 0)
-					former = weak_a.length-1;
-				else
-					former--;
 			}
 			else
 				break;
 		}
 
-		return count;
+		return current;
 	}
 
 	//방문 처리
