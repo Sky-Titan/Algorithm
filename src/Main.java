b6fb41e3f7a6ecc74075a1ed05bb0c007923f7bb
Sky-Titan <er65119@naver.com>
2020-08-25 16:22:40 +0900

    백준 10942

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -15,32 +14,88 @@ public class Main {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-			String binary = br.readLine();
+			int N = Integer.parseInt(br.readLine());
 
-			long multi = 1;
+			int list[] = new int[N+1];
 
-			long result = 0;
+			boolean isPalin[][] = new boolean[N+1][N+1];
 
-			long subtract = 0;
+			StringTokenizer strtok = new StringTokenizer(br.readLine());
 
-			long count = 0;
-			for(int i = binary.length()-1; i >= 0; i--)
+			for(int i = 1;i < list.length; i++)
+				list[i] = Integer.parseInt(strtok.nextToken());
+
+			for(int k = 1;k <= N;k++)
 			{
-				int num = binary.charAt(i) - 48;
+				int i = 0;
 
-				if(count != 0 && count % 3 == 0)
+				while(1 <= k - i || k + i <= N)
 				{
-					multi *= 10;
-					subtract += 3;
-				}
+					int index1 = k - i;
+					int index2 = k + i;
 
-				if(num == 1)
-					result += Math.pow(2, count - subtract) * multi;
-				count++;
+					//k 기준으로 좌측
+					if(1 <= index1)
+					{
+						if(k - index1 < 2)
+						{
+							if(list[index1] == list[k])
+								isPalin[index1][k] = true;
+						}
+						else
+						{
+							if(list[index1] == list[k] && isPalin[index1+1][k-1])
+								isPalin[index1][k] = true;
+						}
+					}
+
+					//k 기준 우측
+					if(index2 <= N)
+					{
+						if(index2 - k < 2)
+						{
+							if(list[k] == list[index2])
+								isPalin[k][index2] = true;
+						}
+						else
+						{
+							if(list[k] == list[index2] && isPalin[k+1][index2-1])
+								isPalin[k][index2] = true;
+						}
+					}
+
+					//k 기준 양측
+					if(1 <= index1 && index2 <= N)
+					{
+						if(index2 - index1 < 2)
+						{
+							if(list[index1] == list[index2])
+								isPalin[index1][index2] = true;
+						}
+						else
+						{
+							if(list[index1] == list[index2] && isPalin[index1+1][index2-1])
+								isPalin[index1][index2] = true;
+						}
+					}
+
+					i++;
+				}
 			}
 
-			System.out.println(result);
+			int M = Integer.parseInt(br.readLine());
 
+			for(int i = 0;i<M;i++)
+			{
+				strtok = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(strtok.nextToken());
+				int b = Integer.parseInt(strtok.nextToken());
+
+				if(isPalin[a][b])
+					bw.write("1\n");
+				else
+					bw.write("0\n");
+			}
 
 			bw.close();
 		}
