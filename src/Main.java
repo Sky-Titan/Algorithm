9662f0a9248cf286c6bdaa51031776e62fca736d
Sky-Titan <er65119@naver.com>
2020-08-26 17:24:29 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -11,33 +11,28 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int T = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		for(int t = 0;t < T;t++)
+
+		for(int i = 0;i < N;i++)
 		{
-			int N = Integer.parseInt(br.readLine());
+			String str = br.readLine();
 
-			StringBuilder list[] = new StringBuilder[N];
-			Trie trie = new Trie();
+			boolean visited[] = new boolean[26];
+			int sum = 2015;
 
-			for(int i = 0;i < N;i++)
+			for(int j = 0;j < str.length();j++)
 			{
-				list[i] = new StringBuilder(br.readLine());
-				trie.insertString(list[i]);
-			}
-
-			for(int i = 0;i < N;i++)
-			{
-				if(!trie.isConsistent(list[i]))
+				if(!visited[str.charAt(j) - 'A'])
 				{
-					bw.write("NO\n");
-					break;
+					sum -= str.charAt(j);
+					visited[str.charAt(j) - 'A'] = true;
 				}
-
-				if(i == N-1)
-					bw.write("YES\n");
 			}
+
+			bw.write(sum+"\n");
 		}
+
 		bw.close();
 	}
 
@@ -53,62 +48,5 @@ public class Main {
 		}
 	}
 
-	static class Trie{
-
-		Node root = new Node();
-
-		public Trie()
-		{
-
-		}
-
-		public void insertString(StringBuilder str)
-		{
-			Node node = root;
-
-			for(int i = 0;i < str.length();i++)
-			{
-				char c = str.charAt(i);
-
-				node.children.putIfAbsent(c, new Node());
-				node = node.children.get(c);
-
-				if(i == str.length()-1)
-					node.isFinish = true;
-			}
-		}
-
-		public boolean isConsistent(StringBuilder str)
-		{
-			Node node = root;
-
-			for(int i = 0;i < str.length();i++)
-			{
-				char c = str.charAt(i);
-
-				node = node.children.get(c);
-
-				//아직 끝에 도달하지 않았는데 문자열이 존재한다면(접두어) 일관성 파괴
-				if(i < str.length()-1)
-				{
-					if(node.isFinish)
-						return false;
-				}
-			}
-
-			return true;
-		}
-	}
-
-	static class Node{
-
-		HashMap<Character, Node> children = new HashMap<>();
-		boolean isFinish = false;//현재까지의 문자열이 존재하는 지 판별
-
-		public Node()
-		{
-
-		}
-	}
 
 }
