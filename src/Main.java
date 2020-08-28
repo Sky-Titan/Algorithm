68ab022c82d35f0e1d49c544175e5bf636cf0cd5
Sky-Titan <er65119@naver.com>
2020-08-28 23:20:36 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -6,44 +6,7 @@ import java.util.*;
 
 public class Main {
 
-	static void solution() throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-
-		int M = Integer.parseInt(br.readLine());
-
-		StringBuilder text = new StringBuilder(br.readLine());
-
-		int count = 0;
-
-		int length = 0;
-
-		for(int i = 0;i < text.length() - 2;)
-		{
-			if(text.charAt(i) == 'I' && text.charAt(i+1) == 'O' && text.charAt(i+2) == 'I')
-			{
-				length++;
-				if(length == N)
-				{
-					length--;
-					count++;
-				}
-				i+=2;
-			}
-			else
-			{
-				length = 0;
-				i++;
-			}
-		}
-
-		bw.write(count+"");
-
-		bw.close();
-	}
 
 
 
@@ -51,7 +14,11 @@ public class Main {
 
 		try
 		{
-			solution();
+			Solution s = new Solution();
+
+			String expression = "50*6-3*2";
+
+			System.out.println(s.solution(expression));
 		}
 		catch (Exception e)
 		{
--- src/Solution.java
+++ src/Solution.java
@@ -3,8 +3,140 @@ import java.util.*;
 
 class Solution {
 
-	public int[] solution(String[] gems) {
-		int[] answer = {};
-		return answer;
+	static String op_list[] = {"+","-","*"};
+	static boolean visited[] = new boolean[3];
+
+	static long max = Long.MIN_VALUE;
+
+	public static long solution(String expression) {
+
+		String[] operators = new String[3];
+
+		for(int i = 0;i < 3;i++)
+		{
+			operators[0] = op_list[i];
+			visited[i] = true;
+			recursive(1, 1, expression, operators);
+			visited[i] = false;
+		}
+
+
+		return max;
+	}
+
+	static void recursive(int depth, int op_index, String expression, String[] operators)
+	{
+
+		if(depth == 3)
+		{
+			op_index = 0;
+
+			while(!expression.contains(operators[op_index]))
+				op_index++;
+
+
+			StringTokenizer strtok = new StringTokenizer(expression, operators[op_index]);
+
+			System.out.println(Arrays.toString(operators));
+			String[] list = new String[strtok.countTokens()];
+
+			int k = 0;
+			while(strtok.hasMoreTokens())
+				list[k++] = strtok.nextToken();
+
+			long result = 0;
+
+			if(operators[op_index].equals("*"))
+				result = 1;
+
+			for(int i = 0;i < list.length;i++)
+			{
+				long value = calculate(list[i],op_index+1, operators);
+
+				if(operators[op_index].equals("+"))
+					result += value;
+				else if(operators[op_index].equals("-"))
+					result -= value;
+				else
+					result *= value;
+			}
+
+			System.out.println(result);
+			max = Math.max(max, Math.abs(result));
+		}
+		else
+		{
+			for(int i = 0;i < op_list.length;i++)
+			{
+				if(!visited[i])
+				{
+					visited[i] = true;
+					operators[op_index] = op_list[i];
+					recursive(depth+1, op_index+1, expression, operators);
+					visited[i] = false;
+				}
+			}
+
+		}
+	}
+
+	static long calculate(String expression, int op_index, String[] operators)
+	{
+		if(expression.contains("+") || expression.contains("-") || expression.contains("*"))
+		{
+			long result = 0;
+
+			while(!expression.contains(operators[op_index]))
+				op_index++;
+
+			if(op_index >= op_list.length)
+				return 0;
+
+			if(operators[op_index].equals("*"))
+				result = 1;
+
+			StringTokenizer strtok = new StringTokenizer(expression, operators[op_index]);
+
+			String[] list = new String[strtok.countTokens()];
+
+			int k = 0;
+
+			while(strtok.hasMoreTokens())
+				list[k++] = strtok.nextToken();
+
+			for(int i = 0;i < list.length;i++)
+			{
+				if(list[i].contains("+") || list[i].contains("-") || list[i].contains("*"))
+				{
+					long value = calculate(list[i], op_index + 1, operators);
+
+					if(operators[op_index].equals("+"))
+						result += value;
+					else if(operators[op_index].equals("-"))
+						result -= value;
+					else
+						result *= value;
+				}
+				else
+				{
+					long value = Integer.parseInt(list[i]);
+
+					System.out.println("원자 "+value);
+					if(operators[op_index].equals("+"))
+						result += value;
+					else if(operators[op_index].equals("-"))
+						result -= value;
+					else
+						result *= value;
+				}
+
+			}
+			return result;
+		}
+		else
+		{
+			System.out.println("원자 "+expression);
+			return Integer.parseInt(expression);
+		}
 	}
 }
