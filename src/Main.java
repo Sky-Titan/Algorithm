b911145f586fb7b194e7acd5e31e240b4d600851
Sky-Titan <er65119@naver.com>
2020-09-03 23:27:20 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javafx.geometry.Pos;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -11,61 +13,127 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-
-		int list[] = new int[N];
-
 		StringTokenizer strtok = new StringTokenizer(br.readLine());
 
-		for(int i = 0;i < N;i++)
-			list[i] = Integer.parseInt(strtok.nextToken());
+		int N = Integer.parseInt(strtok.nextToken());
+		int M = Integer.parseInt(strtok.nextToken());
 
-		Arrays.sort(list);
+		int[][] map = new int[N+1][M+1];
 
-		int M = Integer.parseInt(br.readLine());
+		int x[] = {-1, 1, 0, 0};
+		int y[] = {0, 0, -1, 1};
 
-		strtok = new StringTokenizer(br.readLine());
-		for(int i = 0;i < M;i++)
+		for(int i = 1;i <= N;i++)
 		{
-			int a = Integer.parseInt(strtok.nextToken());
+			String row = br.readLine();
 
-			if(binarySearch(list,a))
-				bw.write("1\n");
-			else
-				bw.write("0\n");
+			for(int j = 1;j <= M;j++)
+			{
+				map[i][j] = row.charAt(j-1)-'0';
+			}
 		}
 
+
+
+		int distance = bfs(map, N, M);
+
+		bw.write(distance+"");
 		bw.close();
 	}
 
-	//list 안에 a가 존재하는지 확인
-	static boolean binarySearch(int[] list, int a)
+	static int bfs(int[][] map, int N,int M)
 	{
-		//나올 수 있는 최소 index
-		int left = 0;
+		boolean visited[][][] = new boolean[N+1][M+1][2];
 
-		//나올 수 있는 최대 index
-		int right = list.length-1;
+		Queue<Position> queue = new LinkedList<>();
+		queue.offer(new Position(1, 1, 1, false));
+		visited[1][1][0] = true;
+		visited[1][1][1] = true;
 
-		while(left <= right)
+		int result = Integer.MAX_VALUE;
+
+		while(!queue.isEmpty())
 		{
-			int mid = (left + right) / 2;
+			Position now = queue.poll();
 
-			if(list[mid] == a)
-				return true;
-			else if(list[mid] > a)
-				right = mid - 1;
-			else
-				left = mid + 1;
+			if(now.x == N && now.y == M)
+			{
+				result = Math.min(result, now.distance);
+			}
+
+			int x[] = {-1, 1, 0, 0};
+			int y[] = {0, 0, -1, 1};
+
+			for(int i = 0;i < x.length;i++)
+			{
+				int next_x = now.x + x[i];
+				int next_y = now.y + y[i];
+
+				if(0 < next_x && next_x <= N && 0 < next_y && next_y <= M)
+				{
+					//안 깬놈
+					if(!now.destory)
+					{
+						if(!visited[next_x][next_y][0])
+						{
+							visited[next_x][next_y][0] = true;
+
+							if(map[next_x][next_y] == 0)
+								queue.offer(new Position(next_x, next_y, now.distance+1, now.destory));
+							else
+								queue.offer(new Position(next_x, next_y, now.distance+1, true));
+						}
+					}
+					//벽 깨고 온 놈
+					else
+					{
+						if(!visited[next_x][next_y][1])
+						{
+							visited[next_x][next_y][1] = true;
+
+							if(map[next_x][next_y] == 0)
+								queue.offer(new Position(next_x, next_y, now.distance+1, now.destory));
+						}
+					}
+				}
+			}
 		}
 
-		return false;
+		if(result == Integer.MAX_VALUE)
+			return -1;
+		return result;
 	}
 
 
+	static class Position{
+
+		int x, y, distance;
+		boolean destory = false;
+
+		public Position(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public Position(int x, int y, int distance, boolean destory) {
+			this.x = x;
+			this.y = y;
+			this.distance = distance;
+			this.destory = destory;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Position pos = (Position)obj;
+
+			if(x == pos.x && y == pos.y)
+				return true;
+			return false;
+		}
+	}
+
 
 	public static void main(String[] args) {
-
 		try
 		{
 			solution();
