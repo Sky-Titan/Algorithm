8ae494c5238ba2af9cff32b77f1e630b2cd65cfd
Sky-Titan <er65119@naver.com>
2020-09-18 16:08:38 +0900

    백준 드래곤 커브

--- src/Main.java
+++ src/Main.java
@@ -7,143 +7,141 @@ import java.util.*;
 
 public class Main {
 
-	static boolean visited[][];
-	static int count = 0;
+
+	static int map[][] = new int[101][101];
 
 	public static void solution() throws Exception
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String[] list = br.readLine().split(" ");
-		int N = Integer.parseInt(list[0]);
-		int M = Integer.parseInt(list[1]);
+		int N = Integer.parseInt(br.readLine());
 
-
-		list = br.readLine().split(" ");
-
-		int start_x = Integer.parseInt(list[0]);
-		int start_y = Integer.parseInt(list[1]);
-		int start_dir = Integer.parseInt(list[2]);
-
-		int map[][] = new int[N][M];
+		ArrayList[] dragonCurves = new ArrayList[N];
 
 		for(int i = 0;i < N;i++)
 		{
-			list = br.readLine().split(" ");
+			dragonCurves[i] = new ArrayList<Position>();
 
-			for(int j = 0;j < M;j++)
-				map[i][j] = Integer.parseInt(list[j]);
+			String[] list = br.readLine().split(" ");
+			int x = Integer.parseInt(list[0]);
+			int y = Integer.parseInt(list[1]);
+			int d = Integer.parseInt(list[2]);
+			int g = Integer.parseInt(list[3]);
+
+			dragonCurves[i].add(new Position(x, y));
+			makeDragonCurve(dragonCurves[i], d, g);
 		}
 
-		visited = new boolean[N+1][M+1];
+		int count = 0;
 
-		Queue<Position> queue = new LinkedList<>();
-		queue.offer(new Position(start_x, start_y, start_dir, 1));
-		visited[start_x][start_y] = true;
-
-		while (!queue.isEmpty())
+		for(int i = 0;i < 100;i++)
 		{
-			Position now = queue.poll();
-			int dir = now.dir;
-
-			count = now.count;
-
-			boolean go_back = true;
-
-			for(int i = 0;i < 4;i++)
+			for(int j = 0;j < 100;j++)
 			{
-				dir = turnLeft(dir);
-
-				int next[] = getFront(now.x, now.y, dir);
-
-				if(0 <= next[0] && next[0] < N && 0 <= next[1] && next[1] < M)
-				{
-					if(!visited[next[0]][next[1]] && map[next[0]][next[1]] == 0)
-					{
-						visited[next[0]][next[1]] = true;
-						queue.offer(new Position(next[0], next[1], dir,now.count + 1));
-						go_back = false;
-						break;
-					}
-				}
-			}
-
-			//네방향 모두 갈 수 없는 경우 뒤돌아가야함
-			if(go_back)
-			{
-				int next[] = getBack(now.x, now.y, now.dir);
-				if(0 <= next[0] && next[0] < N && 0 <= next[1] && next[1] < M)
-				{
-					//벽이 아닌 경우에만
-					if (map[next[0]][next[1]] == 0)
-						queue.offer(new Position(next[0],next[1], now.dir, now.count));
-				}
+				if(map[i][j] == 1 && map[i+1][j] == 1 && map[i][j+1] == 1 && map[i+1][j+1] == 1)
+					count++;
 			}
 		}
 
 		bw.write(count+"");
-
 		bw.close();
 		br.close();
 	}
 
-	public static int turnLeft(int dir)
+	public static void makeDragonCurve(ArrayList<Position> dragonCurve, int d, int g)
 	{
-		if(dir == 0)
-			return 3;
-		return dir - 1;
-	}
+		//0세대 드래곤 커브 만들기
+		map[dragonCurve.get(0).x][dragonCurve.get(0).y] = 1;
 
-	public static int[] getBack(int x, int y, int dir)
-	{
-		int next[] = {x, y};
-
-		//북
-		if(dir == 0)
-			next[0] ++;
-			//동
-		else if(dir == 1)
-			next[1] --;
-			//남
-		else if(dir == 2)
-			next[0] --;
+		//x증가 방향 -> 남쪽
+		if(d == 0)
+		{
+			map[dragonCurve.get(0).x + 1][dragonCurve.get(0).y] = 1;
+			dragonCurve.add(new Position(dragonCurve.get(0).x + 1, dragonCurve.get(0).y));
+		}
+		//y감소 방향 -> 서쪽
+		else if(d == 1)
+		{
+			map[dragonCurve.get(0).x][dragonCurve.get(0).y - 1] = 1;
+			dragonCurve.add(new Position(dragonCurve.get(0).x, dragonCurve.get(0).y - 1));
+		}
+		//x감소 방향 -> 북쪽
+		else if(d == 2)
+		{
+			map[dragonCurve.get(0).x - 1][dragonCurve.get(0).y] = 1;
+			dragonCurve.add(new Position(dragonCurve.get(0).x - 1, dragonCurve.get(0).y));
+		}
+		//y증가 방향 -> 동쪽
 		else
-			next[1] ++;
-		return next;
+		{
+			map[dragonCurve.get(0).x][dragonCurve.get(0).y + 1] = 1;
+			dragonCurve.add(new Position(dragonCurve.get(0).x, dragonCurve.get(0).y + 1));
+		}
+
+		//g세대 만큼
+		for(int i = 0;i < g;i++)
+		{
+			//기준점은 끝점 기준
+			int size = dragonCurve.size();
+			int ref_x = dragonCurve.get(size - 1).x;
+			int ref_y = dragonCurve.get(size - 1).y;
+
+			//회전하게 되면 첫 시작점의 회전점이 다음 끝점이 되어야함
+			for(int j = size - 2;j >= 0;j--)
+			{
+				int turn_pos[] = turnPosition(ref_x, ref_y, dragonCurve.get(j).x, dragonCurve.get(j).y);
+				map[turn_pos[0]][turn_pos[1]] = 1;
+				dragonCurve.add(new Position(turn_pos[0], turn_pos[1]));
+			}
+		}
+
 	}
 
-	public static int[] getFront(int x, int y, int dir)
+	public static int[] turnPosition(int ref_x, int ref_y, int x, int y)
 	{
-		int next[] = {x, y};
+		int turn_pos[] = new int[2];
 
-		//북
-		if(dir == 0)
-			next[0] --;
-		//동
-		else if(dir == 1)
-			next[1] ++;
-		//남
-		else if(dir == 2)
-			next[0] ++;
+		//주의 : 반시계 방향으로 회전 (이유 : 예시의 좌표계는 y가 가로줄 담당, x가 세로줄 담당 -> 여기서는 x가 가로줄 담당, y가 세로줄 담당이어서 거꾸로
+
+		//+ + -> + -
+		if(ref_x > x && ref_y > y)
+		{
+			turn_pos[0] = ref_x + Math.abs(ref_y - y);
+			turn_pos[1] = ref_y - Math.abs(ref_x - x);
+		}
+		//+ - -> - -
+		else if(ref_x > x && ref_y <= y)
+		{
+			turn_pos[0] = ref_x - Math.abs(ref_y - y);
+			turn_pos[1] = ref_y - Math.abs(ref_x - x);
+
+		}
+		//- - -> - +
+		else if(ref_x <= x && ref_y <= y)
+		{
+			turn_pos[0] = ref_x - Math.abs(ref_y - y);
+			turn_pos[1] = ref_y + Math.abs(ref_x - x);
+		}
+		//- + -> + +
 		else
-			next[1] --;
-		return next;
-	}
+		{
+			turn_pos[0] = ref_x + Math.abs(ref_y - y);
+			turn_pos[1] = ref_y + Math.abs(ref_x - x);
+		}
 
+		return turn_pos;
+	}
 
 	static class Position{
-		int x, y, dir;
-		int count;
+		int x, y;
 
-		public Position(int x, int y, int dir, int count) {
+		public Position(int x, int y)
+		{
 			this.x = x;
 			this.y = y;
-			this.dir = dir;
-			this.count = count;
 		}
 	}
-
 
 	public static void main(String[] args) {
 		try
