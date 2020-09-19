da4f1b4fa0099688b932c50f5a522b128aa00085
Sky-Titan <er65119@naver.com>
2020-09-19 15:44:39 +0900

    카드섞기 백준

--- src/Main.java
+++ src/Main.java
@@ -7,248 +7,52 @@ import java.util.*;
 
 public class Main {
 
-	static int map[][];
-
-	static int cases[] = {0, 4, 2, 4, 4, 1};
-	static ArrayList<Position> cameras = new ArrayList<>();
-	static int min = Integer.MAX_VALUE;
+	static int S[], P[], card[];
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void solution() throws Exception
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+
+		S = new int[N];
+		P = new int[N];
+		card = new int[N];
 
 		String[] list = br.readLine().split(" ");
 
-		int N = Integer.parseInt(list[0]);
-		int M = Integer.parseInt(list[1]);
-
-		map = new int[N][M];
-
 		for(int i = 0;i < N;i++)
 		{
-			list = br.readLine().split(" ");
-
-			for(int j = 0;j < M;j++)
-			{
-				map[i][j] = Integer.parseInt(list[j]);
-				if(0 < map[i][j] && map[i][j] < 6)
-					cameras.add(new Position(i, j));
-			}
+			P[i] = Integer.parseInt(list[i]);
+			card[i] = i % 3;
 		}
-		findAllCases(0, N, M);
 
-		if(min == Integer.MAX_VALUE)
-			bw.write("0");
-		else
-			bw.write(min+"");
-		bw.close();
-		br.close();
-	}
+		list = br.readLine().split(" ");
 
-	static void findAllCases(int camera, int N, int M)
-	{
-		if(camera == cameras.size())
+		for(int i = 0;i < N;i++)
+			S[i] = Integer.parseInt(list[i]);
+
+		int count = 0;
+
+		while(!Arrays.equals(card, P))
 		{
-			int count = 0;
+			int temp[] = card.clone();
 
+			//i번째 위치의 카드를 S[i] 위치로
 			for(int i = 0;i < N;i++)
-			{
-				for(int j = 0;j < M;j++)
-				{
-					if(map[i][j] == 0)
-						count++;
-				}
-			}
-			min = Math.min(min, count);
-		}
-		else
-		{
-			int camera_number = map[cameras.get(camera).x][cameras.get(camera).y];
-			Stack<Position> recover_stack = new Stack<>();
+				card[i] = temp[S[i]];
 
-			for(int i = 0;i < cases[camera_number];i++)
+			//이미 존재하는 조합 -> 사이클 존재 -> 절대 못구함
+			if(count > 120119)
 			{
-				makeMap(cameras.get(camera).x, cameras.get(camera).y, i, camera_number, recover_stack);
+				bw.write("-1");
+				return;
+			}
 
-				findAllCases(camera + 1, N, M);
+			count++;
+		}
 
-				recoverMap(recover_stack);
-			}
-		}
-	}
-
-	//카메라 타입에 따라서 감시영역 지정
-	static void makeMap(int x, int y, int index, int camera_number, Stack<Position> recover_stack)
-	{
-		//1번 카메라
-		if(camera_number == 1)
-		{
-			setObservation(x, y, index, recover_stack);
-		}
-		else if(camera_number == 2)
-		{
-			//남북
-			if(index == 0)
-			{
-				setObservation(x, y, 2, recover_stack);
-				setObservation(x, y, 3, recover_stack);
-			}
-			//동서
-			else
-			{
-				setObservation(x, y, 0, recover_stack);
-				setObservation(x, y, 1, recover_stack);
-			}
-		}
-		else if(camera_number == 3)
-		{
-			//북동
-			if(index == 0)
-			{
-				setObservation(x, y, 3, recover_stack);
-				setObservation(x, y, 0, recover_stack);
-			}
-			//동남
-			else if(index == 1)
-			{
-				setObservation(x, y, 0, recover_stack);
-				setObservation(x, y, 2, recover_stack);
-			}
-			//남서
-			else if(index == 2)
-			{
-				setObservation(x, y, 2, recover_stack);
-				setObservation(x, y, 1, recover_stack);
-			}
-			//서북
-			else
-			{
-				setObservation(x, y, 1, recover_stack);
-				setObservation(x, y, 3, recover_stack);
-			}
-		}
-		else if(camera_number == 4)
-		{
-			//서북동
-			if(index == 0)
-			{
-				setObservation(x, y, 1, recover_stack);
-				setObservation(x, y, 3, recover_stack);
-				setObservation(x, y, 0, recover_stack);
-			}
-			//북동남
-			else if(index == 1)
-			{
-				setObservation(x, y, 3, recover_stack);
-				setObservation(x, y, 0, recover_stack);
-				setObservation(x, y, 2, recover_stack);
-			}
-			//동남서
-			else if(index == 2)
-			{
-				setObservation(x, y, 0, recover_stack);
-				setObservation(x, y, 2, recover_stack);
-				setObservation(x, y, 1, recover_stack);
-			}
-			//남서북
-			else
-			{
-				setObservation(x, y, 2, recover_stack);
-				setObservation(x, y, 1, recover_stack);
-				setObservation(x, y, 3, recover_stack);
-			}
-		}
-		else
-		{
-			for(int i = 0;i < 4;i++)
-				setObservation(x, y, i, recover_stack);
-		}
-	}
-
-	//원래대로 복구
-	public static void recoverMap(Stack<Position> recover_stack)
-	{
-		while(!recover_stack.isEmpty())
-		{
-			Position p = recover_stack.pop();
-			map[p.x][p.y] = 0;
-		}
-	}
-
-	//감시 영역 지정
-	public static void setObservation(int x, int y, int index, Stack<Position> recover_stack)
-	{
-		//동
-		if(index == 0)
-		{
-			for(int j = y + 1;j < map[x].length;j++)
-			{
-				//감시영역 지정
-				if(map[x][j] == 0)
-				{
-					map[x][j] = -1;
-					recover_stack.push(new Position(x, j));
-				}
-				//벽이면 멈춤
-				else if(map[x][j] == 6)
-					break;
-			}
-		}
-		//서
-		else if(index == 1)
-		{
-			for(int j = y - 1;j >= 0;j--)
-			{
-				//감시영역 지정
-				if(map[x][j] == 0)
-				{
-					map[x][j] = -1;
-					recover_stack.push(new Position(x, j));
-				}
-				else if(map[x][j] == 6)
-					break;
-			}
-		}
-		//남
-		else if(index == 2)
-		{
-			for(int i = x + 1;i < map.length;i++)
-			{
-				//감시영역 지정
-				if(map[i][y] == 0)
-				{
-					map[i][y] = -1;
-					recover_stack.push(new Position(i, y));
-				}
-				else if(map[i][y] == 6)
-					break;
-			}
-		}
-		//북
-		else
-		{
-			for(int i = x - 1;i >= 0;i--)
-			{
-				//감시영역 지정
-				if(map[i][y] == 0)
-				{
-					map[i][y] = -1;
-					recover_stack.push(new Position(i, y));
-				}
-				else if(map[i][y] == 6)
-					break;
-			}
-		}
-	}
-
-	static class Position{
-		int x, y;
-
-		public Position(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
+		bw.write(count+"");
 	}
 
 
@@ -256,6 +60,9 @@ public class Main {
 		try
 		{
 			solution();
+
+			bw.close();
+			br.close();
 		}
 		catch (Exception e)
 		{
