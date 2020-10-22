ee42cb9c98a48df8dab324a01f0c1d93e878a0a7
Sky-Titan <er65119@naver.com>
2020-10-22 13:46:47 +0900

    .

--- kt_algo/src/main/kotlin/Main.kt
+++ kt_algo/src/main/kotlin/Main.kt
@@ -21,8 +21,57 @@ fun solution() {
 
     list.sortDescending()
 
+    var result = 0
 
+    var i = 0
 
+    while(i < N && list[i] > 0)
+    {
+        if(i == N - 1)
+        {
+            result += list[i]
+            i++
+        }
+        else
+        {
+            if(list[i] * list[i + 1] > list[i] + list[i + 1])
+            {
+                result += list[i] * list[i + 1]
+                i += 2
+            }
+            else
+            {
+                result += list[i]
+                i++
+            }
+        }
+    }
+
+    i = N - 1
+
+    while(i >= 0 && list[i] < 0)
+    {
+        if(i == 0)
+        {
+            result += list[i]
+            i--
+        }
+        else
+        {
+            if(list[i] * list[i - 1] > list[i] + list[i - 1])
+            {
+                result += list[i] * list[i - 1]
+                i -= 2
+            }
+            else
+            {
+                result += list[i]
+                i--
+            }
+        }
+
+    }
+    bw.write(result.toString())
 }
 
 
