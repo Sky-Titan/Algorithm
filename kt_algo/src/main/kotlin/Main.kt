1721525e2582f341c15a5c5d38f8420a0916b5e5
Sky-Titan <er65119@naver.com>
2020-11-03 20:44:35 +0900

    .

--- kt_algo/src/main/kotlin/Main.kt
+++ kt_algo/src/main/kotlin/Main.kt
@@ -3,15 +3,13 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
-<<<<<<< HEAD
-=======
+
 import kotlin.collections.ArrayList
 
 import kotlin.collections.HashMap
 import kotlin.collections.HashSet
 import kotlin.math.max
 
->>>>>>> de5cec63033b3727b3d0dd505863274133f59d98
 import kotlin.math.min
 
 var br = BufferedReader(InputStreamReader(System.`in`))
@@ -22,59 +20,6 @@ fun solution() {
 
     var strtok = StringTokenizer(br.readLine())
 
-<<<<<<< HEAD
-    var N = strtok.nextToken().toInt()
-    var S = strtok.nextToken().toInt()
-
-    strtok = StringTokenizer(br.readLine())
-
-    var list = Array(N, {strtok.nextToken().toInt()})
-
-    //구간합의 최소값
-    var min_length = Int.MAX_VALUE
-
-    var before = 0
-    var after = 0
-
-    var sum = list[0]
-
-    while(true)
-    {
-        //S이상인 수이면 min 값 구하기
-        if(sum >= S)
-        {
-            min_length = min(min_length, after - before + 1)
-
-            if(before < after)
-            {
-                sum -= list[before]
-                before ++
-            }
-            //before == after인 순간 나올 수 있는 최소길이인 1이 되므로 종료
-            else
-                break
-        }
-        else
-        {
-            if(after < N - 1)
-            {
-                after++
-                sum += list[after]
-            }
-            //sum이 s보다 작은데 더이상 값을 추가 못하므로 종료
-            else
-                break
-        }
-
-    }
-
-
-    if(min_length == Int.MAX_VALUE)
-        bw.write("0")
-    else
-        bw.write(min_length.toString())
-}
-=======
     var N = strtok.nextToken().toInt()//정점
     var M = strtok.nextToken().toInt()//간선
 
@@ -100,6 +45,7 @@ fun solution() {
     var end = strtok.nextToken().toInt()
     var K = strtok.nextToken().toInt()
 
+    //교차로 집합
     var mid_set = HashSet<Int>()
 
     for(i in 0 until K)
@@ -115,8 +61,6 @@ fun solution() {
             queue.offer(i)
     }
     count[start][1] = 1
->>>>>>> de5cec63033b3727b3d0dd505863274133f59d98
-
 
     while(!queue.isEmpty())
     {
